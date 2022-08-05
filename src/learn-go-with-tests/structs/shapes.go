36f64b39d0b8b7add9289b7bc02859b3a5d6f967
Edward <edwardrockrw@gmail.com>
2022-08-05 18:27:24 +0800

    refactor with interface

--- src/learn-go-with-tests/structs/shapes.go
+++ src/learn-go-with-tests/structs/shapes.go
@@ -1,14 +1,37 @@
 package structs
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Retangle struct {
-	width  float64
-	height float64
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
 }
 
 func Perimeter(retangle Retangle) float64 {
-	return 2 * (retangle.width + retangle.height)
+	return 2 * (retangle.Width + retangle.Height)
 }
 
-func Area(retangle Retangle) float64 {
-	return retangle.width * retangle.height
+func (r Retangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
 }
--- src/learn-go-with-tests/structs/shapes_test.go
+++ src/learn-go-with-tests/structs/shapes_test.go
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	retangle := Retangle{10.0, 10.0}
-	got := Area(retangle)
-	want := 100.0
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Retangle", shape: Retangle{Width: 10, Height: 10}, hasArea: 100},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 10, Height: 10}, hasArea: 50},
+	}
 
-	if got != want {
-		t.Errorf("got %.2f , want %.2f ", got, want)
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+
+			if got != tt.hasArea {
+				t.Errorf(" %#v got %g want %g ", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
